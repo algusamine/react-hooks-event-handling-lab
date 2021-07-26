d0ce37bd6fad5c1088d4ba8a5522594a97908e88
Amine <algusamine@gmail.com>
2021-07-26 18:35:40 -0400

    late submission

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe(){
+    function onFocusHandler(){
+        console.log("Good!")
+    }
+
+    function onBlurHandler(){
+        console.log("Hey! Eyes on me!")
+    }
+  
+    return (
+        <div>
+            <button onFocus={onFocusHandler} onBlur={onBlurHandler}>Eyes on me</button>
+        </div>
+    )
+}
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,16 @@
 // Code Keypad Component Here
+import React from 'react'
+
+function Keypad(){
+
+    function handleOnChange(){
+        console.log('Entering password...')
+    }
+    return (
+        <div>
+           <input type="password" onChange={handleOnChange}/>
+        </div>
+    )
+}
+
+export default Keypad;
